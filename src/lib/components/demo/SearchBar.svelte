5d09c035c272a1e4535650d78a372b726e04dea2
Daryl Ho <darylhcw@gmail.com>
2023-07-23 05:29:57 +0800

    Fix SearchBar width so it stretches to container.

--- src/lib/components/demo/SearchBar.svelte
+++ src/lib/components/demo/SearchBar.svelte
@@ -3,7 +3,7 @@
   export let query: string = "";
 </script>
 
-<div class="relative flex items-center h-12 drop-shadow-md">
+<div class="relative w-full flex items-center h-12 drop-shadow-md">
   <div class="absolute h-1/2 left-3">
     <SearchIcon fill="slate-500"/>
   </div>
